d82780c72ac21bfbcf6d1b85ddfbb41a023ba07f
SwathiK20 <151981119+SwathiK20@users.noreply.github.com>
2023-11-26 10:06:14 +0530

    Add files via upload
    Day 2

---
+++ Firefox.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("BROWSER VERSION IS: " + browserVersion);
+		System.out.println("BROWSER NAME IS: " + browserName);
+		System.out.println("IS VISIBLE: " + isVisible);
+		System.out.println("RELEASE YEAR: " + releaseYear);
+		System.out.println("BROWSER LOGO: " + browserLogo);
+		
+
+	}
+
+}
---
+++ IsPrime.java
@@ -0,0 +1,29 @@
+package week1.day2;
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+		
+		int num=2;
+		int count=0;
+		
+		if(num>1)
+		{
+			for(int i=1;i<=num;i++)
+			{
+				if(num%i ==0)
+					count++;
+			}
+			if(count==2)
+			{
+				System.out.println("Prime Number");
+			}
+			else
+			{
+				System.out.println("Not Primr Number");
+			}
+		}
+
+	}
+
+}
